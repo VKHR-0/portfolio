377b7c7ed3ad0e5bb62430e16b9d985bfb10750e
0Empty0 <victor.gargat28@gmail.com>
2023-12-10 18:21:43 +0100

    add rendering of project schema

--- src/app/(home)/_components/section-projects.tsx
+++ src/app/(home)/_components/section-projects.tsx
@@ -1,10 +1,25 @@
 import ExpandingText from "@/components/expanding-text";
 import { motion, useScroll, useTransform } from "framer-motion";
+import { useEffect, useState } from "react";
+import { client } from "@/../sanity/lib/client";
+import IProject from "@/types/project";
 
 const SectionProjects = () => {
   const { scrollYProgress } = useScroll();
 
   const width = useTransform(scrollYProgress, [0.25, 0.5], ["50%", "100vw"]);
+
+  const [projects, setProjects] = useState<IProject[]>([]);
+
+  useEffect(() => {
+    const getProjects = async () => {
+      const projects = await client.fetch("*[__type=='project']");
+
+      setProjects(projects);
+    };
+
+    getProjects();
+  }, []);
 
   return (
     <section
@@ -19,6 +34,14 @@ const SectionProjects = () => {
       >
         Projects
       </ExpandingText>
+
+      <div>
+        {projects ? (
+          projects.map((project) => <div key={project._id}>{project._id}</div>)
+        ) : (
+          <p>Not found</p>
+        )}
+      </div>
 
       <motion.div
         className="absolute bottom-0 left-1/2 -translate-x-1/2 bg-zinc-100 h-32"
