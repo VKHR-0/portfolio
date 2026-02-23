d21fcaf46d1aa87cebe8195ecfeceb497b35a82c
0Empty0 <garg.victor11@gmail.com>
2026-02-23 10:56:08 +0200

    change root alias

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,54 @@
-import { createRouter as createTanStackRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import { ConvexQueryClient } from "@convex-dev/react-query";
+import { QueryClient } from "@tanstack/react-query";
+import { createRouter } from "@tanstack/react-router";
+import { routerWithQueryClient } from "@tanstack/react-router-with-query";
+import { ConvexProvider } from "convex/react";
+import { routeTree } from "./routeTree.gen";
 
 export function getRouter() {
-  const router = createTanStackRouter({
-    routeTree,
+	const CONVEX_URL = import.meta.env.VITE_CONVEX_URL;
+	if (!CONVEX_URL) {
+		console.error("missing envar CONVEX_URL");
+	}
 
-    scrollRestoration: true,
-    defaultPreload: 'intent',
-    defaultPreloadStaleTime: 0,
-  })
+	const convexQueryClient = new ConvexQueryClient(CONVEX_URL!);
 
-  return router
+	const queryClient: QueryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				queryKeyHashFn: convexQueryClient.hashFn(),
+				queryFn: convexQueryClient.queryFn(),
+			},
+		},
+	});
+	convexQueryClient.connect(queryClient);
+
+	const router = routerWithQueryClient(
+		createRouter({
+			routeTree,
+
+			scrollRestoration: true,
+			defaultPreload: "intent",
+			defaultPreloadStaleTime: 0,
+
+			context: {
+				queryClient,
+			},
+
+			Wrap: ({ children }) => (
+				<ConvexProvider client={convexQueryClient.convexClient}>
+					{children}
+				</ConvexProvider>
+			),
+		}),
+		queryClient,
+	);
+
+	return router;
 }
 
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: ReturnType<typeof getRouter>
-  }
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: ReturnType<typeof getRouter>;
+	}
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,34 +1,39 @@
 import { TanStackDevtools } from "@tanstack/react-devtools";
-import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
+import type { QueryClient } from "@tanstack/react-query";
+import {
+	createRootRouteWithContext,
+	HeadContent,
+	Scripts,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-
-import ConvexProvider from "@/integrations/convex/provider";
 
 import appCss from "../styles.css?url";
 
-export const Route = createRootRoute({
-	head: () => ({
-		meta: [
-			{
-				charSet: "utf-8",
-			},
-			{
-				name: "viewport",
-				content: "width=device-width, initial-scale=1",
-			},
-			{
-				title: "VKHR | Portfolio",
-			},
-		],
-		links: [
-			{
-				rel: "stylesheet",
-				href: appCss,
-			},
-		],
-	}),
-	shellComponent: RootDocument,
-});
+export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
+	{
+		head: () => ({
+			meta: [
+				{
+					charSet: "utf-8",
+				},
+				{
+					name: "viewport",
+					content: "width=device-width, initial-scale=1",
+				},
+				{
+					title: "VKHR | Portfolio",
+				},
+			],
+			links: [
+				{
+					rel: "stylesheet",
+					href: appCss,
+				},
+			],
+		}),
+		shellComponent: RootDocument,
+	},
+);
 
 function RootDocument({ children }: { children: React.ReactNode }) {
 	return (
@@ -37,20 +42,18 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 				<HeadContent />
 			</head>
 			<body>
-				<ConvexProvider>
-					{children}
-					<TanStackDevtools
-						config={{
-							position: "bottom-right",
-						}}
-						plugins={[
-							{
-								name: "Tanstack Router",
-								render: <TanStackRouterDevtoolsPanel />,
-							},
-						]}
-					/>
-				</ConvexProvider>
+				{children}
+				<TanStackDevtools
+					config={{
+						position: "bottom-right",
+					}}
+					plugins={[
+						{
+							name: "Tanstack Router",
+							render: <TanStackRouterDevtoolsPanel />,
+						},
+					]}
+				/>
 				<Scripts />
 			</body>
 		</html>
