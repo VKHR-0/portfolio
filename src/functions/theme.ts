1f201aa96ab6a9394d38803c6253595b258d24f1
0Empty0 <garg.victor11@gmail.com>
2026-03-19 16:05:06 +0200

    removed dynamic imports

--- src/functions/theme.ts
+++ src/functions/theme.ts
@@ -1,4 +1,5 @@
 import { createServerFn } from "@tanstack/react-start";
+import { getCookie, setCookie } from "@tanstack/react-start/server";
 import * as z from "zod";
 
 const postThemeValidator = z.union([
@@ -10,7 +11,6 @@ export type Theme = z.infer<typeof postThemeValidator>;
 const storageKey = "_preferred-theme";
 
 export const getTheme = createServerFn().handler(async () => {
-	const { getCookie } = await import("@tanstack/react-start/server");
 	const cookie = getCookie(storageKey);
 	return (cookie ?? "system") as Theme;
 });
@@ -18,6 +18,5 @@ export const getTheme = createServerFn().handler(async () => {
 export const setTheme = createServerFn({ method: "POST" })
 	.inputValidator(postThemeValidator)
 	.handler(async ({ data }) => {
-		const { setCookie } = await import("@tanstack/react-start/server");
 		setCookie(storageKey, data);
 	});
